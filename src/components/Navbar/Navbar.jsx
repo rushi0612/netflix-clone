8ac5cc3d7e985e9ffc428619286ba171c1bd012d
Rushikesh <rvp6123@gmail.com>
2026-02-06 23:30:13 +0530

    Add Navbar component with styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png';
+import search_icon from '../../assets/search_icon.svg';
+import bell_icon from '../../assets/bell_icon.svg';
+// import profile_icon from '../../assets/profile_icon.png';
+import profile_img from '../../assets/profile_img.png';
+import caret_icon from '../../assets/caret_icon.svg';
+
 
 const Navbar = () => {
   return (
@@ -17,7 +23,16 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-right">
-        
+        <img src={search_icon} alt="" className="icons" />
+        <p>Children</p>
+        <img src={bell_icon} alt="" className="icons" />
+        <div className="navbar-profile">
+        <img src={profile_img} alt="" className="profile" />
+        <img src={caret_icon} alt="" />
+        <div className="dropdown">
+          <p>Sign Out Of Netflix </p>
+        </div>
+        </div>
       </div>
     </div>
   )
