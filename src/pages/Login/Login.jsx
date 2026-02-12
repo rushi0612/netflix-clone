69ce286243d2ec3c6de50b1bbc27d700b068a66b
Rushikesh <rvp6123@gmail.com>
2026-02-12 09:06:19 +0530

    feat: add sign in page component

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png';
 const Login = () => {
+
+  const [signState, setSignState] = useState("Sign In")
+
   return (
     <div className="login">
-
+      <img src={logo} className="login-logo" alt="" />
+      <div className="login-form">
+        <h1>{signState}</h1>
+        <form >
+          {signState==="Sign Up"?<input type="text" placeholder="Your Name" />:<></>}
+          
+          <input type="email" placeholder="Your Email" />
+          <input type="password" placeholder="Password" />
+          <button>{signState}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+          <div className="form-switch">
+             {signState==="Sign In"?
+            <p>New to Netflix? <span>Sign Up Now</span></p>:<p>Already have account? <span>Sign IN Now</span></p>
+             }
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
