ad535fe59872c0f6585bcf3f4690504d9535ac7e
Rushikesh <rvp6123@gmail.com>
2026-02-13 21:23:25 +0530

    feat: add Firebase integration for user authentication and Firestore database

---
+++ src/firebase.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3ei2j4PGJ_aDTSn1VCdna-onxdL8SskA",
+  authDomain: "netflix-clone-1a1ea.firebaseapp.com",
+  projectId: "netflix-clone-1a1ea",
+  storageBucket: "netflix-clone-1a1ea.firebasestorage.app",
+  messagingSenderId: "562663832372",
+  appId: "1:562663832372:web:d10cd011e106e1baa60361"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password)=>{
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc (collection(db, "user"),{
+            uid:user.uid,
+            name,
+            authProvider: "local",
+            email,
+        })
+    } catch (error) {
+        console.log(error)
+        alert(error);
+    }
+}
